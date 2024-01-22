61944378b83fe51039610fbac41a6efa532a1afe
Dhirachat Chayaporn <omiejung@gmail.com>
2024-01-22 16:09:47 +0700

    Midterm solution - DS 2/2023

---
+++ main.py
@@ -0,0 +1,17 @@
+import MID6318888.q01 as q01
+import MID6318888.q02 as q02
+import MID6318888.q03 as q03
+import MID6318888.q06 as q06
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    q01.start()
+    q02.start()
+    q03.main()
+    q06.main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

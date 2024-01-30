47f52c0825c35bd5f4bafde812148672d4a2cc2e
Dhirachat Chayaporn <omiejung@gmail.com>
2024-01-30 16:16:30 +0700

    Bubble Sort

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import BubbleSort.bubblesort as bubblesort
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    bubblesort.bubblesort()
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
